f9aaba82c8fec0551dff29e94e964d065ecc1320
arm <moungnoi_k@silpakorn.edu>
2021-11-13 21:36:12 +0700

    order

---
+++ database/factories/OrderLogFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\OrderLog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderLogFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = OrderLog::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ resources/js/plugins/swal.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
+
+Vue.prototype.$swal = Swal;
